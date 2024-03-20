a4917ef6a9588a56710d95dd27d966985e880e26
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-03-20 18:18:22 +0200

    Added new 'FormSuccess' cmp

---
+++ components/FormSuccess.tsx
@@ -0,0 +1,16 @@
+import { FaCheck } from "react-icons/fa";
+
+interface FormSuccessProps {
+  message: string;
+}
+
+const FormSuccess = ({ message }: FormSuccessProps) => {
+  return (
+    <div className="flex gap-2 items-center bg-green-400 p-4 rounded text-slate-800">
+      <FaCheck size={20} color="#166534" />
+      {message}
+    </div>
+  );
+};
+
+export default FormSuccess;
