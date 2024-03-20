de1fee9d4db759049eb917e7cd2afa2b9810b818
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-03-20 18:20:37 +0200

    New 'LoginForm' cmp

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { loginFormSchema } from '@/schemas';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import FormError from '@/components/FormError';
+import FormSuccess from '@/components/FormSuccess';
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginFormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="jondoe@mail.com" {...field} type="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormError message="Something went wrong" />
+        <FormSuccess message="All wend well" />
+        <Button type="submit" size="lg" className="w-full">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
